7161b9a14e5b3202396d5e4981bfeebb573544ad
David Rodriguez <david@enyutech.com>
2023-08-13 02:16:58 -0400

    update the NavBar component with color mode toggle switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch.tsx";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="0.5rem">
       <Image src={logo} alt="logo" boxSize="60px" />
-      <Text>SGH</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
