4924d3bddc65ff5c973f3cf2d875ca72f073f0eb
David Rodriguez <david@enyutech.com>
2023-08-20 09:52:20 -0400

    Create custom hook to fetch the games data

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,72 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface GameModel {
-  id: number;
-  slug: string;
-  name: string;
-  released: string;
-  tba: boolean;
-  background_image: string;
-  rating: number;
-  rating_top: number;
-  ratings: {
-    id: number;
-    title: string;
-    count: number;
-    percent: number;
-  }[];
-  ratings_count: number;
-  reviews_text_count: string;
-  added: number;
-  added_by_status: {
-    yet: number;
-    owned: number;
-    beaten: number;
-    toplay: number;
-    dropped: number;
-    playing: number;
-  };
-  metacritic: number;
-  playtime: number;
-  suggestions_count: number;
-  updated: string;
-  esrb_rating: {
-    id: number;
-    name: string;
-    slug: string;
-  };
-  platforms: {
-    platform: {
-      id: number;
-      name: string;
-      slug: string;
-    };
-    released_at: string;
-    requirements: {
-      minimum: string;
-      recommended: string;
-    };
-  };
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: GameModel[];
-}
+import useGames from "../hooks/useGames.ts";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<GameModel[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then(({ data }) => setGames(data.results))
-      .catch((error) => setError(error.message));
-  });
-
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client.ts";
+import { CanceledError } from "axios";
+
+interface GameModel {
+  id: number;
+  slug: string;
+  name: string;
+  released: string;
+  tba: boolean;
+  background_image: string;
+  rating: number;
+  rating_top: number;
+  ratings: {
+    id: number;
+    title: string;
+    count: number;
+    percent: number;
+  }[];
+  ratings_count: number;
+  reviews_text_count: string;
+  added: number;
+  added_by_status: {
+    yet: number;
+    owned: number;
+    beaten: number;
+    toplay: number;
+    dropped: number;
+    playing: number;
+  };
+  metacritic: number;
+  playtime: number;
+  suggestions_count: number;
+  updated: string;
+  esrb_rating: {
+    id: number;
+    name: string;
+    slug: string;
+  };
+  platforms: {
+    platform: {
+      id: number;
+      name: string;
+      slug: string;
+    };
+    released_at: string;
+    requirements: {
+      minimum: string;
+      recommended: string;
+    };
+  };
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: GameModel[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<GameModel[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then(({ data }) => setGames(data.results))
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
